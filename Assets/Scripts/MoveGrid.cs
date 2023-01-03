78f461a1c230543e445459131248db208ee1b19d
Zack Dowd <zdowd2796@gmail.com>
2023-01-03 13:52:32 -0700

    grid spawning, ground mask working

---
+++ Assets/Scripts/MoveGrid.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGrid : MonoBehaviour
+{
+    public MovePoint startPoint;
+    public Vector2Int spawnRange;
+    public Transform spawnParent;
+    public LayerMask groundMask;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateMoveGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GenerateMoveGrid()
+    {
+        for (int x = -spawnRange.x; x <= spawnRange.x; x++)
+        {
+            for (int y = -spawnRange.y; y <= spawnRange.y; y++)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position + new Vector3(x, 10f, y), Vector3.down, out hit, 20f, groundMask))
+                {
+                    Instantiate(startPoint, hit.point, transform.rotation, spawnParent);
+
+                }
+
+            }
+        }
+    }
+}
